7423b2a0e7a49864875a7d2be1002508635f6a03
Ceprnic <ceprnic.josip@gmail.com>
2024-12-10 13:15:57 +0100

    simplify data warehouse

--- seminar/dataWarehouse.sql
+++ seminar/dataWarehouse.sql
@@ -13,12 +13,6 @@ CREATE TABLE dimCustomer (
     State NVARCHAR(255)
 );
 
-CREATE TABLE dimProduct (
-    ProductKey INT IDENTITY(1,1) PRIMARY KEY,
-    ProductID INT,
-    ProductName NVARCHAR(255)
-);
-
 CREATE TABLE dimDeliveryMethod (
     DeliveryMethodKey INT IDENTITY(1,1) PRIMARY KEY,
     DeliveryMethodID INT,
@@ -34,16 +28,22 @@ CREATE TABLE dimDate (
     Day INT
 );
 
+CREATE TABLE dimOrderStatus (
+    OrderStatusKey INT IDENTITY(1,1) PRIMARY KEY,
+    OrderStatusID INT,
+    StatusName NVARCHAR(255)
+);
+
 CREATE TABLE factOrders (
-    SalesKey INT IDENTITY(1,1) PRIMARY KEY,
+    OrderKey INT IDENTITY(1,1) PRIMARY KEY,
     DateKey INT,
     CustomerKey INT,
-    ProductKey INT,
     DeliveryMethodKey INT,
+    OrderStatusKey INT,
     TotalAmount DECIMAL(18,2),
     FOREIGN KEY (DateKey) REFERENCES dimDate(DateKey),
     FOREIGN KEY (CustomerKey) REFERENCES dimCustomer(CustomerKey),
-    FOREIGN KEY (ProductKey) REFERENCES dimProduct(ProductKey),
+    FOREIGN KEY (OrderStatusKey) REFERENCES dimOrderStatus(OrderStatusKey),
     FOREIGN KEY (DeliveryMethodKey) REFERENCES dimDeliveryMethod(DeliveryMethodKey)
 );
 
@@ -51,9 +51,9 @@ INSERT INTO dimCustomer (CustomerID, FirstName, LastName, Email, State)
 SELECT CustomerID, FirstName, LastName, Email, State
 FROM ElectronicsStore.dbo.Customers;
 
-INSERT INTO dimProduct (ProductID, ProductName)
-SELECT ProductID, ProductName
-FROM ElectronicsStore.dbo.Products;
+INSERT INTO dimOrderStatus (OrderStatusID, StatusName)
+SELECT OrderStatusID, StatusName
+FROM ElectronicsStore.dbo.OrderStatus;
 
 INSERT INTO dimDeliveryMethod (DeliveryMethodID, DeliveryMethodName)
 SELECT DeliveryMethodID, Name
@@ -76,17 +76,16 @@ BEGIN
     SET @StartDate = DATEADD(DAY, 1, @StartDate);
 END;
 
-INSERT INTO factOrders (DateKey, CustomerKey, ProductKey, DeliveryMethodKey, TotalAmount)
+INSERT INTO factOrders (DateKey, CustomerKey, DeliveryMethodKey, OrderStatusKey, TotalAmount)
 SELECT 
     d.DateKey,
     c.CustomerKey,
-    p.ProductKey,
     dm.DeliveryMethodKey,
+    os.OrderStatusKey,
     o.TotalAmount
 FROM 
     ElectronicsStore.dbo.Orders o
-JOIN ElectronicsStore.dbo.OrderDetails od ON o.OrderID = od.OrderID
 JOIN dimDate d ON o.OrderDate = d.Date
 JOIN dimCustomer c ON o.CustomerID = c.CustomerID
-JOIN dimProduct p ON od.ProductID = p.ProductID
-JOIN dimDeliveryMethod dm ON o.DeliveryMethodID = dm.DeliveryMethodID;
+JOIN dimDeliveryMethod dm ON o.DeliveryMethodID = dm.DeliveryMethodID
+JOIN dimOrderStatus os ON o.OrderStatusID = os.OrderStatusID;
